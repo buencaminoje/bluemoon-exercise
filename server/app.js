9fb59138f8b77339e05e0a48d42ed1c44bab2bee
jakebuencamino <jake@blumoon.io>
2021-07-12 12:24:20 +0800

    Create app.js

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const server = require('fastify')({ logger: true });
+
+server.get('/', async (request, reply) => {
+  return { message: 'Hello World' };
+});
+
+const start = async () => {
+  try {
+    await server.listen(3000);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
+
+module.exports = server;
