6494b5b5315e0e31e4c658966d4bd8aa079a6467
jakebuencamino <jake@blumoon.io>
2021-07-14 11:10:19 +0800

    Create authorRoutes.js

---
+++ fastify-server/routes/authorRoutes.js
@@ -0,0 +1,39 @@
+const authorRoutes = async (fastify, options) => {
+  const SUCCESS_MESSAGE = 'An author has been succesfully';
+  const ERROR_MESSAGE = "There's a problem in author";
+
+  fastify.post('/authors', async (request, reply) => {
+    let response = await fastify.knex.authorService.create(request.body)
+      .catch(er => {
+        return { responseMessage: `${ERROR_MESSAGE} adding!` };
+      });
+    if (!Object.prototype.hasOwnProperty.call(response, 'responseMessage')) {
+      response = { responseMessage: `${SUCCESS_MESSAGE} added!` };
+    }
+    return response;
+  });
+
+  fastify.get('/authors', async (request, reply) => {
+    const response = await fastify.knex.authorService.get()
+      .catch(er => {
+        return { responseMessage: `${ERROR_MESSAGE} fetching!` };
+      });
+    return response;
+  });
+
+  fastify.put('/authors/:id', async (request, reply) => {
+    let response = await fastify.knex.authorService.update({
+      id: request.params.id,
+      author: request.body
+    })
+      .catch(er => {
+        return { responseMessage: `${ERROR_MESSAGE} updating!` };
+      });
+    if (!Object.prototype.hasOwnProperty.call(response, 'responseMessage')) {
+      response = { responseMessage: `${SUCCESS_MESSAGE} updated!` };
+    }
+    return response;
+  });
+};
+
+module.exports = authorRoutes;
