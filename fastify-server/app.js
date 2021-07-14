5de9d8375f1b80242c0f4e2e2e09edd9832f6c73
jakebuencamino <jake@blumoon.io>
2021-07-14 10:27:05 +0800

    Create app.js

---
+++ fastify-server/app.js
@@ -0,0 +1,25 @@
+const { APP_PORT } = require('./config');
+const fastify = require('fastify')({
+  logger: true
+});
+
+async function startup () {
+  await fastify.register(require('./plugins/db_connection'), {});
+  await fastify.register(require('./services'), {});
+  await fastify.register(require('fastify-formbody'));
+  await fastify.register(require('./routes'), {});
+  await fastify.listen(APP_PORT, function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+  });
+  await fastify.addHook('onClose', (instance, done) => {
+    fastify.knex.destroy();
+    done();
+  });
+
+  return fastify;
+}
+
+module.exports = startup();
