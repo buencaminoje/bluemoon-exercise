60f350d1af4890b54cb0d78dfe3c990887ce39e0
jakebuencamino <jake@blumoon.io>
2021-07-20 16:58:13 +0800

    Update authors.js

--- fastify-server-cli/routes/authors.js
+++ fastify-server-cli/routes/authors.js
@@ -4,6 +4,7 @@ module.exports = async function (fastify, opts) {
   const SUCCESS_MESSAGE = 'An author has been succesfully';
   const ERROR_MESSAGE = "There's a problem in author";
 
+  // Insert new author route
   fastify.post('/authors', async (request, reply) => {
     let response = await fastify.knex.author.create(request.body)
       .catch(er => {
@@ -15,6 +16,7 @@ module.exports = async function (fastify, opts) {
     return response;
   });
 
+  // Get authors route
   fastify.get('/authors', async (request, reply) => {
     const response = await fastify.knex.author.get()
       .catch(er => {
@@ -23,6 +25,7 @@ module.exports = async function (fastify, opts) {
     return response;
   });
 
+  // Update author route
   fastify.put('/authors/:id', async (request, reply) => {
     let response = await fastify.knex.author.update({
       id: request.params.id,
@@ -39,6 +42,7 @@ module.exports = async function (fastify, opts) {
     return response;
   });
 
+  // Delete author route
   fastify.delete('/authors/:id', async (request, reply) => {
     let response = await fastify.knex.author.delete({ id: request.params.id })
       .catch(er => {
